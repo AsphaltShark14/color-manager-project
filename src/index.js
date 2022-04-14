a359b7209a2c29380a69a496291f1e145e0d31aa
Daniel Bogucki <daniel.bogucki14@gmail.com>
2022-04-14 14:20:54 +0200

    Changed useState to Context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import colors from "./color-data.json";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+export const ColorContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorContext.Provider value={{ colors }}>
+      <App/>
+    </ColorContext.Provider>
   </React.StrictMode>
 );
 
